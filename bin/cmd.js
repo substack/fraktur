f55ba15e82018703148337a8eeeb68e6d3e96b3a
James Halliday <mail@substack.net>
2014-11-20 03:24:02 +0100

    decode

--- bin/cmd.js
+++ bin/cmd.js
@@ -3,11 +3,21 @@
 var fraktur = require('../');
 var utf8 = require('utf8-stream');
 var through = require('through2');
+var minimist = require('minimist');
 
-process.stdin.pipe(utf8())
-    .pipe(through(function (buf, enc, next) {
-        this.push(fraktur(buf.toString('utf8')));
-        next();
-    }))
-    .pipe(process.stdout)
-;
+var argv = minimist(process.argv.slice(2), {
+    alias: { d: 'decode' },
+    default: { d: false }
+});
+
+if (argv.decode) {
+}
+else {
+    process.stdin.pipe(utf8())
+        .pipe(through(function (buf, enc, next) {
+            this.push(fraktur(buf.toString('utf8')));
+            next();
+        }))
+        .pipe(process.stdout)
+    ;
+}
